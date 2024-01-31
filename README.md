57aa0f6e83059e0db00097a06f0cc3add4fc8b73
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-01-31 10:35:58 +0700

    Countdown Ramadhan

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Apps Countdown Ramadhan 1445 H
+
+<h2>CountDownRamadhan-APP</h2>
---
+++ js/main.js
@@ -0,0 +1,25 @@
+const countdownDate = new Date("MAR 10, 2024 00:00:00").getTime();
+
+const x = setInterval(function() {
+    const now = new Date().getTime();
+
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // document.write("days", ":", "hours", ":", "minutes", ":", "seconds");
+    // document.write("<br>");
+    // document.write(days, ":", hours, ":", minutes, ":", seconds);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
+
